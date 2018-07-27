92a3f0c3e99804b1dd06d6ad191e94f787de6681
Aneka Happer <anekahapper@Anekas-MBP.home>
2018-07-28 00:12:24 +0100

    unsigned test

--- unsignedtest.c
+++ unsignedtest.c
@@ -2,15 +2,23 @@
 # include <stdio.h>
 
 int main(){
-    const char *a[5] = { "d", "e", "f", "g", "h" };
+    uint32_t a = 5;
+    uint32_t b = 10;
+    
+    uint32_t c = a - b;
 
-    /*a[0] = "d";
+    printf("c is: %u", c);
+
+    /*const char *a[5] = { "d", "e", "f", "g", "h" };
+
+    a[0] = "d";
     a[1] = "e";
     a[2] = "f";
     a[3] = "g";
-    a[4] = "h";*/  
+    a[4] = "h";  
 
 
     printf("this is a: %s", a[2]);
+    */
 
 }
